7400a6958809a18f3d885d88dbdcdd8397481699
kemalreza12 <59290848+kemalreza12@users.noreply.github.com>
2020-07-20 23:26:05 +0700

    Update script.js

--- task2-Condition/script.js
+++ task2-Condition/script.js
@@ -2,15 +2,19 @@ const matematika = 80;
 const bahasaIndonesia = 90;
 const bahasaInggris = 89;
 const ipa = 69;
-const jmlMapel = 4;
 
-let average = (matematika + bahasaIndonesia + bahasaInggris + ipa) / jmlMapel;
-console.log(`Rata-rata = ${average}`);
+if(matematika, bahasaIndonesia, bahasaInggris, ipa === '') {
+    console.log('Semua nilai harus diisi')
+} else {
 
-let grade = "";
-if(average >= 90) grade = "A"
-else if(average >= 80) grade = "B"
-else if(average >= 70) grade = "C"
-else if(average >= 60) grade = "D"
-else grade = "E";
-console.log(`Grade = ${grade}`);
+    let average = (matematika + bahasaIndonesia + bahasaInggris + ipa) / 4;
+    console.log(`Rata-rata = ${average}`);
+    
+    let grade = "";
+    if(average >= 90) grade = "A"
+    else if(average >= 80) grade = "B"
+    else if(average >= 70) grade = "C"
+    else if(average >= 60) grade = "D"
+    else grade = "E";
+    console.log(`Grade = ${grade}`);
+}
